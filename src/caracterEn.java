91afb863b79a0578fdd6bb5c5d2b2562a7ebb193
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-08-31 12:00:14 -0700

    Practica Manejo de Excepciones Programa 1: leer caracter en una dadena Status: Completado

--- src/caracterEn.java
+++ src/caracterEn.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
 public class caracterEn {
+
     public static void main(String[] args) {
-        
+        if (args[0] == null) {
+            System.exit(1);
+        }
+            try {
+                String cadena = args[0];
+                int car = Integer.parseInt(args[1]) - 1;
+
+                System.out.println("Texto: " + cadena);
+                System.out.println("Caracter en la posición " + car + ": " + cadena.charAt(car));
+            }
+         catch (ArrayIndexOutOfBoundsException e){
+             System.out.println(e.getMessage());
+             System.exit(2);
+        }
     }
 }
