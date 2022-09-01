d1e0f1f4b60d73ee758ab5955f158597e676ee43
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-08-31 20:00:52 -0700

    Practica Manejo de Excepciones Programa 1: leer caracter en una dadena Status: Completado Programa 2: metodo main con lectura en terminal Status: Completado Programa 3: excepción número negativo Status: En proceso

--- src/numeroNegativoExcepcion.java
+++ src/numeroNegativoExcepcion.java
@@ -1,8 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.*;
+import java.math.*;
+
 public class numeroNegativoExcepcion {
-    public numeroNegativoExcepcion() {
 
-    }
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double n, r = 0;
+        System.out.println("Número: " );
+        n = s.nextDouble();
+        try{
+            r = Math.sqrt(n);
 
+        } catch (InputMismatchException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("La raiz es: " + r);
     }
 }
